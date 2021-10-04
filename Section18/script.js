71027095ac4d5db8082d053a72449e82f781512f
Marc <marc@jmarcboden.com>
2021-10-04 15:38:52 -0300

    completed

--- Section18/script.js
+++ Section18/script.js
@@ -6,13 +6,52 @@ const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=ddaf8d34b7
 
 const form = document.getElementById('form')
 const search = document.getElementById('search')
+const main = document.getElementById('main')
 //Get initial moview
 getMovies(API_URL)
 
 async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data.results)
+    
+    showMovies(data.results)
+}
+
+function showMovies(movies){
+    main.innerHTML = ''
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview} = movie
+
+        const movieEl = document.createElement
+        ('div')
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+        
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        
+        `
+        main.appendChild(movieEl)
+    })
+}
+
+//set span color for vote average
+function getClassByRate(vote){
+    if(vote >= 8){
+        return 'green'
+    } else if (vote >=5){
+        return 'orange'
+    } else {
+        return 'red'
+    }
 }
 
 form.addEventListener('submit', (e) =>{
